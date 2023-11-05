df89e3667d872e88699150c10dbfdaeabdd8fda8
Benny Nazimov <benick12@gmail.com>
2023-11-05 20:38:41 +0200

    Prettier UI

--- src/components/chat/MessageComp.tsx
+++ src/components/chat/MessageComp.tsx
@@ -1,22 +1,54 @@
+import { Paper, Typography } from "@mui/material";
 import { Message } from "../../types";
-import { Card, Typography } from "@mui/material";
 
 interface MessageProps {
   message: Message;
 }
 
+// converts a unix timestmap to ISO format
+function convertTimestampToISO(timestamp: number) {
+  // format the timestamp to ISO format, example 01/31/2023, 10:10
+  // let formatString = "d/MM/yy, HH:mm";
+  let date = new Date(timestamp);
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let hourFormatted = hour % 12 || 12; // hour returned in 24 hour format
+  let minuteFormatted = minute < 10 ? "0" + minute : minute;
+  return `${hourFormatted}:${minuteFormatted}, ${day}/${month}/${year}`;
+}
+
 export default function MessageComp({ message }: MessageProps) {
   return (
-    <Card>
-      <Typography variant="overline" component="h2">
-        {message.author.$oid}
+    <Paper
+      square
+      sx={{ display: "flex", flexDirection: "column" }}
+      elevation={1}
+    >
+      <Typography
+        sx={{
+          alignSelf: "self-end",
+          color: "#2E3B55",
+          fontSize: "12px",
+        }}
+        margin="5px"
+        fontWeight="bold"
+        variant="h2"
+      >
+        {convertTimestampToISO(message.timestamp)}
       </Typography>
-      <Typography variant="overline" component="h2">
+      <Typography
+        sx={{
+          fontSize: "12px",
+          alignSelf: "self-start",
+        }}
+        margin="5px"
+        variant="h2"
+      >
         {message.text}
       </Typography>
-      <Typography variant="overline" component="h2">
-        {message.timestamp}
-      </Typography>
-    </Card>
+    </Paper>
   );
 }
